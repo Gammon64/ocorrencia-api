6ee57547e8da8a56322139eb10dd381d6e4ebef0
Hugo Henrique <hugo.hbs64@gmail.com>
2024-10-02 16:20:31 -0300

    Revisando e recriando o teste para ClienteService: Testes reescritos de forma mais clara.

--- src/test/java/com/hugo/hbs64/ocorrenciaapi/repositories/ClienteRepositoryTest.java
+++ src/test/java/com/hugo/hbs64/ocorrenciaapi/repositories/ClienteRepositoryTest.java
@@ -1,14 +1,16 @@
 package com.hugo.hbs64.ocorrenciaapi.repositories;
 
-import com.hugo.hbs64.ocorrenciaapi.entities.Cliente;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.Optional;
-import static org.assertj.core.api.Assertions.assertThat;
+import com.hugo.hbs64.ocorrenciaapi.entities.Cliente;
 
 @DataJpaTest
 @ActiveProfiles("test")
