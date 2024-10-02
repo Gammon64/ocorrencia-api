1137ff4cb75b88e47d6bbe956b3cbcfe749a9847
Hugo Henrique <hugo.hbs64@gmail.com>
2024-10-02 15:35:40 -0300

    Revisando teste de entidade e mapper

--- src/test/java/com/hugo/hbs64/ocorrenciaapi/entities/mappers/ClienteMapperTest.java
+++ src/test/java/com/hugo/hbs64/ocorrenciaapi/entities/mappers/ClienteMapperTest.java
@@ -1,20 +1,32 @@
 package com.hugo.hbs64.ocorrenciaapi.entities.mappers;
 
-
-import com.hugo.hbs64.ocorrenciaapi.entities.Cliente;
-import org.junit.jupiter.api.Test;
-
-import static com.hugo.hbs64.ocorrenciaapi.entities.ClienteConstants.CLIENTE_DTO;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.hugo.hbs64.ocorrenciaapi.entities.Cliente;
+import com.hugo.hbs64.ocorrenciaapi.entities.dtos.ClienteDTO;
+
 class ClienteMapperTest {
+    private ClienteDTO clienteDTO;
+
+    @BeforeEach
+    void setUp() {
+        clienteDTO = new ClienteDTO(
+                "John Doe",
+                "12345678901",
+                LocalDate.of(2000, 04, 18));
+    }
 
     @Test
     void mapCliente_withClienteDTO_shouldReturnClienteDTO() {
-        Cliente cliente = ClienteMapper.INSTANCE.fromDto(CLIENTE_DTO);
+        Cliente cliente = ClienteMapper.INSTANCE.fromDto(clienteDTO);
         assertThat(cliente).isNotNull();
-        assertThat(cliente.getNmeCliente()).isEqualTo(CLIENTE_DTO.nmeCliente());
-        assertThat(cliente.getDtaNascimento()).isEqualTo(CLIENTE_DTO.dtaNascimento());
-        assertThat(cliente.getNroCpf()).isEqualTo(CLIENTE_DTO.nroCpf());
+        assertThat(cliente.getNmeCliente()).isEqualTo(clienteDTO.nmeCliente());
+        assertThat(cliente.getDtaNascimento()).isEqualTo(clienteDTO.dtaNascimento());
+        assertThat(cliente.getNroCpf()).isEqualTo(clienteDTO.nroCpf());
     }
 }
